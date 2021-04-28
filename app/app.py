127eeb402193481211117992491cedb9ad7dd1e2
윤현준 <kkkths@naver.com>
2021-04-28 21:38:41 +0900

    update
    show btc chart

--- app/app.py
+++ app/app.py
@@ -1,7 +1,20 @@
 import logging
 from upbit import Upbit
-from flask import Flask, request, render_template
 
+from flask import Flask, send_file, render_template, make_response, request
+
+from io import BytesIO, StringIO
+import numpy as np
+
+## macOS의 경우 아래 순서에 따라서 library를 import해줘야 에러없이 잘 됩니다.
+import matplotlib
+
+# import tensorflow.compat.v1 as tf
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#################
 
 app = Flask(__name__)
 upbit = Upbit()
@@ -9,8 +22,12 @@ upbit.get_hour_candles('KRW-BTC')
 
 
 @app.route('/')
-def root():
-    market = request.args.get('market')
+def normal():
+    return render_template("index.html", width=800, height=600)
+
+
+@app.route('/fig/<market>')  # TODO: xs, ys 구해서 그래프로 만들어야함
+def fig(market):
     if market is None or market == '':
         return 'No market parameter'
 
@@ -18,21 +35,30 @@ def root():
     if candles is None:
         return 'invalid market: {}'.format(market)
 
-    label = market
-    xlabels = []
+    xs = []
+    i = 1
     dataset = []
-    i = 0
+
     for candle in candles:
-        xlabels.append('')
+        xs.append(i)
         dataset.append(candle['trade_price'])
         i += 1
 
-    print("도달")
-    return render_template('index.html', **locals())
+    plt.plot(xs, dataset, 'r-', label=market)
+    plt.legend()
+
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=300)
+    img.seek(0)  ## object를 읽었기 때문에 처음으로 돌아가줌
+    return send_file(img, mimetype='image/png')
+
+
+# plt.savefig(img, format='svg')
+# return send_file(img, mimetype='image/svg')
 
 
 def main():
-    app.run(debug = True)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
