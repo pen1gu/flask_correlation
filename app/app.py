42a69a06e639b5455e556685ee0a1a52e5021e42
윤현준 <kkkths@naver.com>
2021-05-03 14:42:18 +0900

    update file

--- app/app.py
+++ app/app.py
@@ -1,103 +1,102 @@
-import logging
-from upbit import Upbit
-
-from flask import Flask, send_file, render_template, make_response, request
-
-from io import BytesIO, StringIO
-from functools import wraps, update_wrapper
-from datetime import datetime
-## macOS의 경우 아래 순서에 따라서 library를 import해줘야 에러없이 잘 됩니다.
-import matplotlib
-
-# import tensorflow.compat.v1 as tf
-
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-#################
-
-app = Flask(__name__)
-upbit = Upbit()
-
-result_corr = 0.0
-
-
-@app.route('/')
-def normal():  # TODO: 아래 corr 변수에 상관도 분석 값을 넣어야한다.
-    return render_template("index.html", corr=result_corr, width=800, height=600)
-
-
-def nocache(view):
-    @wraps(view)
-    def no_cache(*args, **kwargs):
-        response = make_response(view(*args, **kwargs))
-        response.headers['Last-Modified'] = datetime.now()
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '-1'
-        return response
-
-    return update_wrapper(no_cache, view)
-
-
-@app.route('/fig/<btc_eth>')
-@nocache
-def fig(btc_eth):  # TODO: fig에서 분석한 데이터를 normal로 보내서 render_templates에 뿌려야한다.
-    btc, eth = btc_eth.split("_")
-    btc_candles = upbit.get_days_candles(btc)
-    eth_candles = upbit.get_days_candles(eth)
-
-    if btc_candles is None:
-        return 'invalid market: {}'.format(btc_candles)
-    elif eth_candles is None:
-        return 'invalid market: {}'.format(eth_candles)
-
-    i = 1
-    btc_xs = []
-    btc_dataset = []
-    for candle in btc_candles:
-        btc_xs.append(i)
-        btc_dataset.append(candle['change_rate'])
-        i += 1
-    plt.plot(btc_xs, btc_dataset, 'b-', label=btc)
-
-    i = 1
-    eth_xs = []
-    eth_dataset = []
-    for candle in eth_candles:
-        eth_xs.append(i)
-        eth_dataset.append(candle['change_rate'])
-        i += 1
-
-    plt.plot(eth_xs, eth_dataset, 'r-', label=eth)
-    plt.legend()
-
-    result_corr = np.corrcoef(btc_dataset, eth_dataset)
-
-    img = BytesIO()
-    plt.savefig(img, format='png', dpi=300)
-    img.seek(0)  ## object를 읽었기 때문에 처음으로 돌아가줌
-    return send_file(img, mimetype='image/png')
-
-
-@app.route('/corr')
-def corr():  # TODO: 상관도 분석 완료. 값만 화면 딴에 전달하면 끝
-    plt.plot(result_corr, 'r-', label='')
-
-    img = BytesIO()
-    plt.savefig(img, format='png', dpi=300)
-    img.seek(0)  ## object를 읽었기 때문에 처음으로 돌아가줌
-    return send_file(img, mimetype='image/png')
-
-
-# TODO: 상관도 분석 결과 -> 전체 1 나왔음 : 에러
-
-
-def main():
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
-    main()
+# from upbit import Upbit
+#
+# from flask import Flask, send_file, render_template, make_response, request
+#
+# from io import BytesIO, StringIO
+# from functools import wraps, update_wrapper
+# from datetime import datetime
+# import matplotlib
+#
+# # import tensorflow.compat.v1 as tf
+#
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
+#
+# #################
+#
+# app = Flask(__name__)
+# upbit = Upbit()
+#
+# result_corr = 0.0
+#
+#
+# @app.route('/')
+# def normal():
+#     return render_template("index.html", corr=result_corr, width=800, height=600)
+#
+#
+# def nocache(view):
+#     @wraps(view)
+#     def no_cache(*args, **kwargs):
+#         response = make_response(view(*args, **kwargs))
+#         response.headers['Last-Modified'] = datetime.now()
+#         response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+#         response.headers['Pragma'] = 'no-cache'
+#         response.headers['Expires'] = '-1'
+#         return response
+#
+#     return update_wrapper(no_cache, view)
+#
+#
+# @app.route('/fig/<bit1_bit2>')
+# @nocache
+# def fig(bit1_bit2):  # TODO: fig에서 분석한 데이터를 normal로 보내서 render_templates에 뿌려야한다.
+#     bit1, bit2 = bit1_bit2.split("_")
+#     bit1_candles = upbit.get_days_candles(bit1)
+#     bit2_candles = upbit.get_days_candles(bit2)
+#
+#     if bit1_candles is None:
+#         return 'invalid market: {}'.format(bit1_candles)
+#     elif bit2_candles is None:
+#         return 'invalid market: {}'.format(bit2_candles)
+#
+#     bit1_xs, bit1_dataset = get_bit_data(bit1_candles)
+#     bit2_xs, bit2_dataset = get_bit_data(bit2_candles)
+#
+#     plt.plot(bit1_xs, bit1_dataset, label=bit1)
+#     plt.plot(bit2_xs, bit2_dataset, label=bit2)
+#
+#     # TODO : 원하는 코인 수 만큼 화면에 띄울 수 있도록 유지보수 할 것
+#     plt.legend()
+#
+#     data_frame1 = data = [[], [bit1_dataset], []]
+#     data_frame2 = data = [[], [bit1_dataset], []]
+#
+#     data1 = pd.DataFrame(data_frame1, columns=["high", "Low"], dtype=np.int8)
+#     data2 = pd.DataFrame(data_frame2, dtype=np.int8)
+#
+#     # result_corr = np.corrcoef(bit1_dataset, bit2_dataset)
+#
+#     img = BytesIO()
+#     plt.savefig(img, format='png', dpi=300)
+#     img.seek(0)  ## object를 읽었기 때문에 처음으로 돌아가줌
+#     return send_file(img, mimetype='image/png')
+#
+#
+# def get_bit_data(bit):
+#     i = 1
+#     bit_xs = []
+#     bit_dataset = []
+#     for candle in bit:
+#         bit_xs.append(i)
+#         bit_dataset.append(candle['change_rate'])
+#         i += 1
+#
+#     return bit_xs, bit_dataset
+#
+#
+# # @app.route('/forecast')
+# def forecast(data):  # TODO: 비트코인 주가 예측
+#     high_prices = data['High'].values
+#     low_prices = data['Low'].values
+#     mid_prices = (high_prices + low_prices) / 2
+#     pass
+#
+#
+# # def main():
+# #     app.run(debug=True)
+#
+# # if __name__ == '__main__':
+# #     main()
--- app/main/upbit.py
+++ app/main/upbit.py
@@ -1,22 +1,28 @@
 from upbitpy import Upbitpy
 
+
 class Upbit:
     def __init__(self):
         self.__upbit = Upbitpy()
         self.__krw_markets = self.__get_krw_markets()
 
-
     def __get_krw_markets(self):
         krw_markets = dict()
         all_markets = self.__upbit.get_market_all()
+
         for market in all_markets:
             if market['market'].startswith('KRW-'):
                 krw_markets[market['market']] = market
         return krw_markets
 
-
     def get_days_candles(self, market):
         if market not in self.__krw_markets.keys():
             return None
         candles = self.__upbit.get_days_candles(market, count=10)
+        return candles
+
+    def get_days_many_candles(self, market):
+        if market not in self.__krw_markets.keys():
+            return None
+        candles = self.__upbit.get_days_candles(market, count=100)
         return candles
